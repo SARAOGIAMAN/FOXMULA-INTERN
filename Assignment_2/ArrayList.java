ead2580a3246e358770747f155e7dd93fdbae923
Aman Saraogi <72626970+SARAOGIAMAN@users.noreply.github.com>
2021-06-02 19:12:43 +0530

    Update ArrayList.java

--- Assignment_2/ArrayList.java
+++ Assignment_2/ArrayList.java
@@ -22,7 +22,7 @@ public class ArrayList {
 	}
 	public static void main(String[] args)
 	{
-		ArrayList p=new ArrayList();
+		ArrayList obj=new ArrayList();
 		int a,b,c,d,e;
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter the values to add in an ArrayList ");
@@ -31,13 +31,13 @@ public class ArrayList {
 		c=scanner.nextInt();
 		d=scanner.nextInt();
 		e=scanner.nextInt();
-		p.add(a, b, c, d, e);
+		obj.add(a, b, c, d, e);
 		System.out.println();
 		System.out.println("R   E   S   U   L   T ");
 		System.out.println();
-		System.out.println("ArrayList contains the given value ?     "+p.check(2));
-		System.out.println("ArrayList contains the given value ?     "+p.check(3));
-		System.out.println("ArrayList contains the given value ?     "+p.check(9));
+		System.out.println("ArrayList contains the given value ?     "+obj.check(2));
+		System.out.println("ArrayList contains the given value ?     "+obj.check(3));
+		System.out.println("ArrayList contains the given value ?     "+obj.check(9));
 		scanner.close();
 	}
 }
